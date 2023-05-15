e2e186448de85f9ef9e3818a50271546dc2dd8c8
shahar verbitky <shaharv19@walla.com>
2023-05-15 18:48:45 +0300

    week 4

--- Aiming high Project/Assets/PlayerController.cs
+++ Aiming high Project/Assets/PlayerController.cs
@@ -5,53 +5,147 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    private Animator animator;
+    public Transform transform;
+    public bool IsRight = false;
     public CharacterController controller;
     public Rigidbody rb;
     public Rigidbody wall1;
     public Rigidbody wall2;
     public Rigidbody wall3;
+    public bool IsForward = false;
     public Rigidbody wall4;
     public int speed = 5;
     public Vector2 move;
-    private bool canJump;
+    private bool can_jump = false;
+    private bool is_jumping = false;
     private float currentY = 12.63874f;
-    public float gravity = -9.8f;
+    public float crouchHeight = 8.0f;
+    public float gravity_multiplier = 3.0f;
+    private bool leftMouseTrigged = false;
+    private bool leftMouseReleased = true;
     private Vector3 jump;
     private Vector3 _gravity;
-    public float jump_value = 0.5f;
-    private int check;
+    public float jump_value = 10f;
+    public bool rightClick=false;
+    private float _velocity = 0.0f;
+    Vector3 _move;
+    
+
     // Start is called before the first frame update
+
 
     public void onMove(InputAction.CallbackContext context)
     {
+        //Debug.Log("move");
         move = context.ReadValue<Vector2>();
+       // Debug.Log("right equal to:" + move.x);
+       // Debug.Log("fowrward to:" + move.y);
+        if(move.y==1)
+        {
+            animator.SetBool("isMovingForward", true);
+            IsForward = true;
+        }
+        else
+        {
+            animator.SetBool("isMovingForward", false);
+            IsForward = false;
+        }
+        if(move.x==1)
+        {
+            //animator.SetBool("isMovingRight", true);
+            IsRight = true;
+            Debug.Log("righhhht");
+        }
+        else
+        {
+          //  animator.SetBool("isMovingRight", false);
+            IsRight = false;
+        }
+
+      /*  if (move.x == 1)
+        {
+            animator.SetBool("isMovingRight", false);
+            IsRight = false;
+        }
+        
+        if(move.x==-1)
+        {
+            animator.SetBool("isMovingLeft", true);
+        }
+        else
+        {
+            animator.SetBool("isMovingLeft", false) ;
+        }*/
+        _move = transform.right * move.x + transform.forward * move.y;
     }
-    public void onJump()
+
+    public void crouch()
     {
-        check = 1;
-        Debug.Log("shaloom");
-        jump = new Vector3(0.0f, 1.0f, 0.0f);
-        canJump = true;
-        controller.Move(jump * jump_value*Time.deltaTime);
-        Debug.Log("shaloomB");
-        //  rb.AddForce(jump * jump_value,ForceMode.Impulse);
 
     }
-
-    public void OnDisable()
-    {
-    }
-
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
+    }
+
+    void OnCollisionEnter(UnityEngine.Collision collision)
+    {
+        if (collision.gameObject.name == "Terrain")
+        {
+            can_jump = true;
+            is_jumping = false;
+        }
+
+    }
+    void OnCollisionExit(UnityEngine.Collision collision)
+    {
+        if (collision.gameObject.name == "Terrain")
+        {
+            can_jump = false;
+            is_jumping = true;
+            animator.SetBool("jump", false);
+        }
+
     }
 
     // Update is called once per frame
     void Update()
-    { 
-        controller.Move((transform.right * move.x + transform.forward * move.y) * speed * Time.deltaTime);
-   
+    {
+           if (Input.GetKeyDown("left shift"))
+        {
+            animator.SetBool("isCrouching", true);
+        }
+        if (Input.GetKeyUp("left shift"))
+        {
+            animator.SetBool("isCrouching", false);
+
+        }
+        if (Input.GetMouseButtonDown(0))
+        {
+            animator.SetBool("rightMouseClick", true);
+            rightClick = true;
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            animator.SetBool("rightMouseClick", false);
+            rightClick = false;
+        }
+
+        if (Input.GetButtonDown("Jump") && can_jump == true)
+        {
+            _move.y = 22.5f;
+            is_jumping = true;
+            animator.SetBool("jump", true);
+        }
+        controller.Move(_move * speed * Time.deltaTime);
+        if (is_jumping == true)
+        {
+            _move.y = _move.y - 1.8f;
+           // controller.Move(_move * speed * Time.deltaTime);
+        }
+       /// controller.Move(_move * speed * Time.deltaTime);
+        //  controller.Move(_move * speed * Time.deltaTime);
         /*if ((rb.position.y-currentY>0 && rb.position.y-currentY<0.1)|| rb.position.y<currentY)
         {
             rb.useGravity = false;
@@ -62,18 +156,7 @@ public class PlayerController : MonoBehaviour
         wall2.isKinematic = true;
         wall3.isKinematic = true;
         wall4.isKinematic = true;
-        if (check == 1)
-        {
-            Debug.Log("shaloomC");
-            check--;
-        }
-        
-    }
-    void movePlayer()
-    {
 
-        /* Vector3 movment = new Vector3(move.x, 0f, move.y).normalized;
-          //transform.Translate(movment * speed * Time.deltaTime, Space.World);
-         rb.velocity=(movment * speed * Time.deltaTime)*100;*/
     }
 }
+
--- Aiming high Project/Assets/cameraSet.cs
+++ Aiming high Project/Assets/cameraSet.cs
@@ -6,24 +6,98 @@ public class cameraSet : MonoBehaviour
 {
     public Transform player;
 
+    public bool isAltPressed=false;
+    private bool wasMoving = false;
+    public float letsSee = 0.4f;
+    public float walkOffset = 0.3f;
     public float mouseSensivity = 100f;
+    private int count = 1;
+    private int countA = 1;
+    private int countB = 1;
+    public int frames = 1;
+    public int setFrames = 100;
+    public float yroatet = 30f;
+
     float xRoatation = 0f;
+    [SerializeField] PlayerController controller;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        Cursor.lockState = CursorLockMode.Locked;
+       Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
     void Update()
     {
-        float mouseX = Input.GetAxis("Mouse X") * mouseSensivity * Time.deltaTime;
-        float mouseY = Input.GetAxis("Mouse Y") * mouseSensivity * Time.deltaTime;
-        xRoatation -= mouseY;
-        xRoatation = Mathf.Clamp(xRoatation, -90f, 90f);
-        transform.localRotation = Quaternion.Euler(xRoatation, 0f, 0f);
-        player.Rotate(Vector3.up * mouseX);
+         float mouseX = Input.GetAxis("Mouse X") * mouseSensivity * Time.deltaTime;
+         float mouseY = Input.GetAxis("Mouse Y") * mouseSensivity * Time.deltaTime;
+         xRoatation -= mouseY;
+        if (controller.rightClick == true)
+        {
+            xRoatation = Mathf.Clamp(xRoatation, -22.5f, 22.5f);
+        }
+        else
+        {
+            xRoatation = Mathf.Clamp(xRoatation, -45f, 45f);
+        }
+         transform.localRotation = Quaternion.Euler(xRoatation, 0f, yroatet);
+         player.Rotate(Vector3.up * mouseX);
+        if (count == 1)
+        {
+            if (Input.GetKeyDown("left shift"))
+            {
+                transform.position = transform.position + new Vector3(0, -1.0f,0.2f);
+                Debug.Log("shift");
+                isAltPressed = true;
+            }
+            count = 0;
+        }
+
+        if (count == 0)
+        {
+            if (Input.GetKeyUp("left shift"))
+            {
+                // controller.height = currentY;
+                transform.position = transform.position + new Vector3(0, 1.0f, -0.2f);
+                Debug.Log("shift realsised");
+                isAltPressed = false;
+            }
+            count = 1;
+        }
+
+         /* if (controller.IsForward==true && countA==1)
+          {
+            player.position = player.position + new Vector3(0, 0, 1f);
+            //transform.position= transform.position + new Vector3(0,0, walkOffset);
+            countA = 0;
+              Debug.Log("here");
+          }
+
+          else if (controller.IsForward == false && countA == 0)
+          {
+               player.position = player.position + new Vector3(0, 0, -1f);
+              //transform.position = transform.position + new Vector3(0, 0, -walkOffset);
+              countA = 1;
+              Debug.Log("here2");
+          }
+          /*
+        if (controller.IsRight == true && countB== 1)
+        {
+            transform.position = transform.position + new Vector3(walkOffset+0.08f, 0, walkOffset/2+0.08f);
+            countB = 0;
+            Debug.Log("here");
+        }
+
+        else if (controller.IsRight == false && countB == 0)
+        {
+            transform.position = transform.position + new Vector3(-walkOffset - 0.08f, 0, -walkOffset / 2 - 0.08f);
+            countB = 1;
+            Debug.Log("here2");
+        }
+         */
+
 
     }
 }
